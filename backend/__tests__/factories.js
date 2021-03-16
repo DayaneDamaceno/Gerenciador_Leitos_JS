ec2db806e0ccfcce08f77068d06e0f0f929d1083
Dayane Damaceno <dayane.damaceno@hotmail.com>
2021-03-16 16:24:22 -0300

    Testes concluidos de 4 requisitos

--- backend/__tests__/factories.js
+++ backend/__tests__/factories.js
@@ -1,10 +1,33 @@
 import faker from 'faker';
 import { factory } from 'factory-girl';
+import * as cnpjValidates from '@fnando/cnpj';
+import * as cpfValidates from '@fnando/cpf';
 
 import Hospital from '../src/app/models/Hospital';
+import HospitalBed from '../src/app/models/HospitalBed';
+import Patient from '../src/app/models/Patient';
 
 factory.define('Hospital', Hospital, {
   name: faker.company.companyName(),
+  cnpj: cnpjValidates.generate(true),
+  password: faker.internet.password,
+  qtd_uti: faker.random.number(10),
+  qtd_enfermaria: faker.random.number(10),
+});
+
+factory.define('Leito', HospitalBed, {
+  type: 'UTI',
+  status: 'vago',
+  hospital_id: factory.assoc('Hospital', 'id'),
+});
+
+factory.define('Paciente', Patient, {
+  name: faker.company.companyName(),
+  hospital_id: factory.assoc('Hospital', 'id'),
+  leito_id: factory.assoc('Leito', 'id'),
+  cpf: cpfValidates.generate(true),
+  data_nascimento: faker.date.past(),
+  estado: 'Grave',
 });
 
 export default factory;
--- backend/__tests__/integration/user.test.js
+++ backend/__tests__/integration/user.test.js
@@ -1,24 +1,75 @@
 import request from 'supertest';
-// import bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
-// import factory from '../factories';
+import factory from '../factories';
 import truncate from '../util/truncate';
 
+// - Cadastrar hospitais = OK
+// - Criptar a senha = OK
+// - Login do hospital = OK
+// - Authentication do hospital - OK
+// - Cadastrar paciente no leito
+// - Buscar paciente por CPF
+// - Alterar dados do paciente
+// - Liberar leito
+// - Listar todos os pacientes do hospital
+
 describe('Hospital', () => {
+  let token;
   beforeEach(async () => {
     await truncate();
   });
+  beforeAll(async (done) => {
+    const { cnpj, password } = await factory.create('Hospital');
+    request(app)
+      .post('/sessions')
+      .send({ cnpj, password })
+      .end((err, response) => {
+        token = response.body.token;
+        done();
+      });
+  });
 
   it('should be able to register', async () => {
-    const response = await request(app).post('/hospitals').send({
-      name: 'Hospital Teste',
-      cnpj: '69.968.241/0001-25',
-      password: 'acesso',
-      qtd_uti: 5,
-      qtd_enfermaria: 10,
-    });
+    const hospital = await factory.attrs('Hospital');
+    const response = await request(app).post('/hospitals').send(hospital);
 
     expect(response.body).toHaveProperty('id');
+  });
+  it('should encrypt hospital password when new user created', async () => {
+    const hospital = await factory.create('Hospital', {
+      password: '123456',
+    });
+
+    const comparePassword = await bcrypt.compare(
+      '123456',
+      hospital.password_hash
+    );
+
+    expect(comparePassword).toBe(true);
+  });
+  it('should be able to logging', async () => {
+    const { cnpj, password } = await factory.create('Hospital');
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({ cnpj, password });
+
+    expect(response.body).toHaveProperty('token');
+  });
+  it('should be authorized with a token', async () => {
+    const response = await request(app)
+      .get('/hospitals')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(200);
+  });
+  it('should be able to register a patient', async () => {
+    const patient = await factory.create('Paciente');
+
+    // console.log(patient);
+
+    expect();
   });
 });
